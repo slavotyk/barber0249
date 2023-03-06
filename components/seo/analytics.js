518e2cab57d02935cf8e12d68a2dc48f0bffdf5e
vdyachenko777 <v.dyachenko@offsidegaming.com>
2023-03-06 11:47:49 +0100

    First commit

--- components/seo/analytics.js
+++ components/seo/analytics.js
@@ -4,19 +4,19 @@ const Analytics = () => {
 
     return (
         <>
-            <Script
-                id='gtm-script'
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NLJPJK5');
-`
-                }}
-            />
+{/*            <Script*/}
+{/*                id='gtm-script'*/}
+{/*                strategy="afterInteractive"*/}
+{/*                dangerouslySetInnerHTML={{*/}
+{/*                    __html: `*/}
+{/*                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':*/}
+{/*new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],*/}
+{/*j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=*/}
+{/*'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);*/}
+{/*})(window,document,'script','dataLayer','GTM-NLJPJK5');*/}
+{/*`*/}
+{/*                }}*/}
+{/*            />*/}
 
             <Script
                 id='ga-script'
@@ -26,7 +26,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-J3GNNY3VKB', {
+            gtag('config', 'G-Q14MMPLKGB', {
               page_path: window.location.pathname,
             });
           `,
